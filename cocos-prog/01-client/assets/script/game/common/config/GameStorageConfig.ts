4ac10c08a51d72116b73c09368b742884acebe27
cyybhzw <50670854+cyybhzw@users.noreply.github.com>
2025-01-11 17:23:30 +0800

    cocosprog

---
+++ cocos-prog/01-client/assets/script/game/common/config/GameStorageConfig.ts
@@ -0,0 +1,7 @@
+/** 本地存储名配置 */
+export enum GameStorageConfig {
+    /** 当前语言类型 */
+    Language = "Language",
+    /** 玩家帐号名 */
+    AccountName = "AccountName"
+}
---
+++ cocos-prog/01-client/assets/script/view/UIRoot.ts
@@ -0,0 +1,26 @@
+import { _decorator, Component, Node , Canvas , sys , log} from 'cc';
+// import * as cc from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('UIRoot')
+export class UIRoot extends Component {
+    @property({ type: Canvas })
+    private Canvas = null;
+
+    start() {
+        console.log("当前平台" + sys.platform)
+        console.log("当前系统语言" + sys.language)
+
+        let winSize = cc.winSize;
+        log("适配比例 ： " + (winSize.height / winSize.width))
+        if(winSize.height / winSize.width <= 1.8){
+            // this.Canvas.fitHeight = true
+            // this.Canvas.fitWidth = false
+        }
+    }
+
+    // update(deltaTime: number) {
+        
+    // }
+}
+
